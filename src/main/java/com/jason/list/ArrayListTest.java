3ecf3ed0f7de8f36432d944cf440f7e2e062b03c
陈梦 <chenmeng@chenmengdeMacBook-Pro.local>
2017-09-04 16:24:53 +0800

    Add arraylist extends abstractList

---
+++ src/main/java/com/jason/list/ArrayListTest.java
@@ -0,0 +1,13 @@
+package com.jason.list;
+
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+
+        arrayList.add("Hello ");
+        arrayList.add("world!");
+
+        System.out.println(arrayList.toString());
+    }
+}
